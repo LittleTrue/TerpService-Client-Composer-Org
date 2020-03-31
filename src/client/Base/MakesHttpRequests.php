7a60f02dcab26776f8e56c44a656b190c8099a7c
root <644812368@qq.com>
2020-03-31 15:46:45 +0800

    发版依赖要求向下兼容PHP56, 修复对接message索引问题

--- src/client/Base/MakesHttpRequests.php
+++ src/client/Base/MakesHttpRequests.php
@@ -45,7 +45,7 @@ trait MakesHttpRequests
         }
 
         $result = json_decode($response->getBody()->getContents(), true);
-
+        
         switch ($result['code']) {
             /* 3002、3003 <==> 无效、过期 */
             case 3002:
@@ -54,11 +54,11 @@ trait MakesHttpRequests
                 $credential = $this->app['credential'];
                 $credential->setToken('');
                 $credential->token();
-                throw new ClientError($result['message'], $result['code']);
+                throw new ClientError($result['msg'], $result['code']);
             case 200:
                 return $result;
             default:
-                throw new ClientError($result['message'], $result['code']);
+                throw new ClientError($result['msg'], $result['code']);
         }
     }
 }
